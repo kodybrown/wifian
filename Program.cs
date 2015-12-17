4c91ec340e1f219cf056121d010c1f39ebdf6fdb
Kody Brown <thewizard@wasatchwizard.com>
2015-12-16 21:03:48 -0700

    Updated the borders a bit. Added a header that is also sortable by ssid or signal strength.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿//
+//
 // Copyright (C) 2013 Kody Brown (kody@bricksoft.com).
 // 
 // MIT License:
@@ -38,10 +38,18 @@ namespace WifiAnalyzer
 		private static ConsoleColor indexColor = ConsoleColor.DarkGray;
 
 		private static ConsoleColor color1 = ConsoleColor.Green;
-		private static ConsoleColor color2 = ConsoleColor.DarkGreen;
-		private static ConsoleColor color3 = ConsoleColor.Magenta;
-		private static ConsoleColor color4 = ConsoleColor.Red;
-		private static ConsoleColor color5 = ConsoleColor.DarkRed;
+		private static ConsoleColor color2 = ConsoleColor.Green;
+		private static ConsoleColor color3 = ConsoleColor.DarkGreen;
+		private static ConsoleColor color4 = ConsoleColor.DarkGreen;
+		private static ConsoleColor color5 = ConsoleColor.Red;
+		private static ConsoleColor color6 = ConsoleColor.DarkRed;
+
+		public enum SortBy
+		{
+			NotSet = 0,
+			Name,
+			Signal
+		}
 
 		public static void Main( string[] args )
 		{
@@ -51,9 +59,17 @@ namespace WifiAnalyzer
 			int ethIndex = 0;
 			int netIndex = 0;
 
+			SortBy sortBy = SortBy.Name;
+			bool shift = false;
+			int sortModifier = -1;
+
 			List<Wlan.WlanBssEntry> wlanBssEntries = new List<Wlan.WlanBssEntry>();
 			ConsoleKeyInfo keyPressed = new ConsoleKeyInfo();
 			Dictionary<int, string> ethNames = new Dictionary<int, string>();
+
+			// '┼' '─'
+			string captionSeparator = string.Format("────{0,-" + colSsid + "}─┼─{1,-" + colSignal + "}─┼─{2}──────"
+				, new string('─', colSsid), new string('─', colSignal), new string('─', colBarLen));
 
 			ethIndex = 0;
 			foreach (WlanClient.WlanInterface wlanIface in client.Interfaces) {
@@ -77,20 +93,31 @@ namespace WifiAnalyzer
 
 					//foreach (WlanClient.WlanInterface wlanIface in client.Interfaces) {
 					for (ethIndex = 0; ethIndex < client.Interfaces.Length; ethIndex++) {
+						WlanClient.WlanInterface wlanIface = client.Interfaces[ethIndex];
+
 						Console.ForegroundColor = highlightColor;
 						Console.WriteLine("\neth" + ethIndex + " \"" + ethNames[ethIndex] + "\":");
 						//Console.WriteLine(wlanIface.InterfaceName + ":");
 						ethIndex++;
 						Console.ForegroundColor = defaultColor;
+						Console.WriteLine("    {0,-" + colSsid + "}   {1,-" + colSignal + "}   {2}", "[S]SID" + (sortBy == SortBy.Name ? (shift ? " «" : " »") : ""), "Signal", "Signal S[t]rength" + (sortBy == SortBy.Signal ? (shift ? " «" : " »") : ""));
+						Console.WriteLine(captionSeparator);
 
 						wlanBssEntries.Clear();
 						wlanBssEntries.AddRange(wlanIface.GetNetworkBssList());
 
-						wlanBssEntries.Sort(delegate( Wlan.WlanBssEntry a, Wlan.WlanBssEntry b )
-						{
-							return string.Compare(ASCIIEncoding.ASCII.GetString(a.dot11Ssid.SSID),
-								ASCIIEncoding.ASCII.GetString(b.dot11Ssid.SSID), StringComparison.InvariantCultureIgnoreCase);
-						});
+						if (sortBy == SortBy.Name) {
+							sortModifier = shift ? -1 : 1;
+							wlanBssEntries.Sort(delegate ( Wlan.WlanBssEntry a, Wlan.WlanBssEntry b ) {
+								return sortModifier * string.Compare(ASCIIEncoding.ASCII.GetString(a.dot11Ssid.SSID),
+									ASCIIEncoding.ASCII.GetString(b.dot11Ssid.SSID), StringComparison.InvariantCultureIgnoreCase);
+							});
+						} else if (sortBy == SortBy.Signal) {
+							sortModifier = shift ? 1 : -1;
+							wlanBssEntries.Sort(delegate ( Wlan.WlanBssEntry a, Wlan.WlanBssEntry b ) {
+								return sortModifier * a.rssi.CompareTo(b.rssi);
+							});
+						}
 
 						netIndex = 0;
 						foreach (Wlan.WlanBssEntry network in wlanBssEntries) {
@@ -109,7 +136,26 @@ namespace WifiAnalyzer
 					if (exit) {
 						break;
 					}
-					if (keyPressed.Key == ConsoleKey.P || keyPressed.Key == ConsoleKey.Spacebar) {
+
+					if (keyPressed.Key == ConsoleKey.S) {
+						if (sortBy != SortBy.Name) {
+							// switching from a different sort column
+							shift = false;
+						} else {
+							shift = !shift;
+						}
+						sortBy = SortBy.Name;
+						keyPressed = new ConsoleKeyInfo();
+					} else if (keyPressed.Key == ConsoleKey.T) {
+						if (sortBy != SortBy.Signal) {
+							// switching from a different sort column
+							shift = false;
+						} else {
+							shift = !shift;
+						}
+						sortBy = SortBy.Signal;
+						keyPressed = new ConsoleKeyInfo();
+					} else if (keyPressed.Key == ConsoleKey.P || keyPressed.Key == ConsoleKey.Spacebar) {
 						Console.Write("\nPAUSED: Press any key to continue");
 						keyPressed = Console.ReadKey(true);
 						if (keyPressed.Key == ConsoleKey.Q || keyPressed.Key == ConsoleKey.Escape) {
@@ -118,6 +164,7 @@ namespace WifiAnalyzer
 						}
 						keyPressed = new ConsoleKeyInfo();
 					} else {
+						//Console.WriteLine("\nPress [S] to change sort and [R] to reverse.");
 						Console.WriteLine("\nPress [Space] to pause or [Escape] to quit.");
 					}
 				}
@@ -134,15 +181,17 @@ namespace WifiAnalyzer
 			Console.ForegroundColor = defaultColor;
 		}
 
+		private static int colSsid = 25;
+		private static int colSignal = 8;
+		private static int colBarLen = 25;
+
 		private static void WriteNetwork( int netIndex, Wlan.WlanBssEntry network )
 		{
-			int mzxSsidLen = 25;
-			int barLength = 25;
 			string ssidName = ASCIIEncoding.ASCII.GetString(network.dot11Ssid.SSID).Trim('\0');
-			string bar = CreateBar(network, barLength);
+			string bar = CreateBar(network, colBarLen);
 
-			if (ssidName.Length > mzxSsidLen) {
-				ssidName = ssidName.Substring(0, mzxSsidLen).Trim();
+			if (ssidName.Length > colSsid) {
+				ssidName = ssidName.Substring(0, colSsid).Trim();
 			} else if (ssidName.Length == 0) {
 				ssidName = "<unknown/hidden>";
 			}
@@ -150,18 +199,20 @@ namespace WifiAnalyzer
 			Console.ForegroundColor = indexColor;
 			Console.Write("{0,-3}", netIndex);
 			Console.ForegroundColor = defaultColor;
-			Console.Write(" {0,-" + mzxSsidLen + "}|Signal: {1,4:###} dBm |", ssidName, network.rssi);
+			Console.Write(" {0,-" + colSsid + "} │ {1," + colSignal + ":###} │ ", ssidName, network.rssi + " dBm");
 
-			if (network.linkQuality > 90) {
+			if (network.linkQuality > 85) {
 				Console.ForegroundColor = color1;
-			} else if (network.linkQuality > 80) {
+			} else if (network.linkQuality > 65) {
 				Console.ForegroundColor = color2;
-			} else if (network.linkQuality > 60) {
+			} else if (network.linkQuality > 50) {
 				Console.ForegroundColor = color3;
-			} else if (network.linkQuality > 30) {
+			} else if (network.linkQuality > 35) {
 				Console.ForegroundColor = color4;
-			} else {
+			} else if (network.linkQuality > 20) {
 				Console.ForegroundColor = color5;
+			} else {
+				Console.ForegroundColor = color6;
 			}
 
 			Console.Write(bar);
@@ -175,8 +226,20 @@ namespace WifiAnalyzer
 			float barWidth = (float)barLength / 100;
 			uint linkQuality = network.linkQuality;
 			int barValue = (int)(linkQuality * barWidth);
-			// '▄' '▀' '▐' '█'
-			return string.Format("{0}{1}", new string('▄', barValue), new string(' ', barLength - barValue));
+			//return string.Format("{0}{1}", new string('▄', barValue), new string(' ', barLength - barValue));
+
+			// '▄' '▀' '▐' '█' '≡' '═'
+			char ch;
+
+			//if (linkQuality > 70) {
+			//	ch = '≡';
+			//} else {
+			//	ch = '═';
+			//}
+			ch = '═';
+
+			return string.Format("{0}{1}", new string(ch, barValue), new string(' ', barLength - barValue));
 		}
 	}
 }
+
